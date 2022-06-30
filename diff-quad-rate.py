f66dd91ad9e66f69a22df10c60249fd3e407b00a
Wenzheng Kuang <wkuang1@nd.edu>
2022-06-29 21:20:43 -0400

    update the level ordering and output of diff-quad-rate

--- diff-quad-rate.py
+++ diff-quad-rate.py
@@ -3,7 +3,7 @@ from netgen.geom2d import unit_square
 from netgen.csg import *
 import matplotlib.pyplot as plt
 from prol import *
-from mymg import *
+from prol.mymg import *
 from ngsolve.la import EigenValues_Preconditioner
 from ngsolve.krylovspace import CGSolver
 
@@ -23,6 +23,7 @@ else:
     mesh = Mesh(unit_cube.GenerateMesh(maxh=1/4))
     maxdofs=3e6
 ne = mesh.ne
+
 
 # HDG
 V = VectorL2(mesh) 
@@ -79,7 +80,7 @@ else:
              -uhat*r*n+q*n*vhat
           + lam*alpha/h*(u-uhat)*(v-vhat))*dx(element_boundary=True,
                                           intrules={TRIG:ir})
-
+# heat-source in inner subdomain
 f = LinearForm(fes)
 if dim == 2:
     f += ((1 + 1/2 * sin(x) * sin(y)) * (16 * (x - x**2) * (y - y**2)) * v
@@ -97,7 +98,7 @@ elif dim == 3:
           + 32 * (x - x**2) * (z - z**2) * v
           - 8 * sin(x) * (x - x**2) * (z - z**2) * (cos(y) * (1 - 2 * y) - 2 * sin(y)) * v
           + 32 * (x - x**2) * (y - y**2) * v
-          + 16 * sin(x) * sin(y) * (x - x**2) * (y - y**2) * v) * dx(intrules = {QUAD:ir_c})
+          + 16 * sin(x) * sin(y) * (x - x**2) * (y - y**2) * v) * dx(intrules = {TRIG:ir_c})
     u_exact = 16 * x * (1- x) * y * (1 - y) * z * (1 - z)
     q_exact = CF((1 + 1/2 * sin(x) * sin(y)) * (16 * (y - y**2) * (z - z**2) * (1 - 2 * x), 16 * (x - x**2) * (z - z**2) * (1 - 2 * y), 
                   16 * (x - x**2) * (y - y**2) * (1 - 2 * z)))
@@ -111,6 +112,7 @@ pre = MultiGrid(a_ax.mat, prol, nc=M.ndof,
 
 gfu = GridFunction(fes)
 uhath, qh, uh = gfu.components
+# vtk = VTKOutput(ma=mesh,coefs=[uh], names=["sol"], filename="adpt"+str(dim),subdivision=0)
 
 def SolveBVP():
     fes.Update()
@@ -140,33 +142,78 @@ def SolveBVP():
     gfu.vec.data += a.inner_solve * f.vec
     print("IT: ", it, "cond: ", max(lams)/min(lams), "NDOFS: ", M.ndof)
 
+l = []    # l = list of estimated total error
 
-# SolveBVP()
+# postprocessing
+VT = HDiv(mesh, order=0, discontinuous=True)
+WT = H1(mesh)
+qhs = GridFunction(VT, "flux")
+uhs = GridFunction(WT, "primal")
+qt, rt = VT.TnT()
+at = BilinearForm(VT)
+at += qt*n*rt*n*dx(element_boundary=True)
+ft = LinearForm(VT)
+ft += (qh*n-lam*alpha/h*(uh-uhath))*rt*n*dx(element_boundary=True, intrules={SEGM:ir})
+
+
+
+# a posterior error estimator
+def CalcError():
+    # compute the flux:
+    VT.Update()      
+    qhs.Update()
+    at.Assemble()
+    ft.Assemble()
+    qhs.vec.data = at.mat.Inverse()*ft.vec
+    
+    WT.Update()
+    uhs.Update()
+    uhs.Set(uh)
+    # compute estimator:
+    err = 1/lam*(qh-qhs)**2 + lam*(qh/lam-grad(uhs))**2
+    eta2 = Integrate(err, mesh, VOL, element_wise=True)
+    maxerr = max(eta2)
+    l.append ((fes.ndof, sqrt(sum(eta2))))
+#     print("ndof =", fes.ndof, " maxerr =", maxerr**0.5)
+    
+    # mark for refinement:
+    for el in mesh.Elements():
+        mesh.SetRefinementFlag(el, eta2[el.nr] > 0.25*maxerr)
+
+
+level = 0
+print('==============================')
+print(f'DIM: {dim}, Smoother Type: {smType}, Smooth Steps: {smStep}, Block Smoother: {block}')
+print('==============================')
+print(f'level: {level}')
+SolveBVP()
+L2_uErr =  sqrt(Integrate((uh - u_exact)*(uh - u_exact), mesh))
+L2_qErr =  sqrt(Integrate((qh - q_exact)*(qh - q_exact), mesh))
+print(f"uh L2-error: {L2_uErr:.3E}")
+print(f"qh L2-error: {L2_qErr:.3E}")
+print('==============================')
+prev_uErr, prev_qErr = L2_uErr, L2_qErr
 with TaskManager():
-    level = 1
-    prev_uErr, prev_qErr = 0, 0
     u_rate, q_rate = 0, 0
-    print('==============================')
-    print(f'DIM: {dim}, Smoother Type: {smType}, Smooth Steps: {smStep}, Block Smoother: {block}')
-    print('==============================')
     while M.ndof < maxdofs:  
-        if dim == 2:
-            mesh.ngmesh.Refine()
-        else:
-            mesh.Refine(onlyonce = True)
-            #mesh.ngmesh.Refine()
+        # CalcError()
+        # if dim == 2:
+        #     mesh.ngmesh.Refine()
+        # else:
+        #     mesh.Refine(onlyonce = True)
+        mesh.ngmesh.Refine()
         # NOTE the different refinement!!!
-        meshRate = 2 if dim == 2 else sqrt(2)
+        meshRate = 2 # if dim == 2 else sqrt(2)
 
         level += 1
         SolveBVP()
         # ===== convergence check =====
         L2_uErr =  sqrt(Integrate((uh - u_exact)*(uh - u_exact), mesh))
         L2_qErr =  sqrt(Integrate((qh - q_exact)*(qh - q_exact), mesh))
-        if level != 2:
-            u_rate = log(prev_uErr / L2_uErr) / log(meshRate)
-            q_rate = log(prev_qErr / L2_qErr) / log(meshRate)
+        u_rate = log(prev_uErr / L2_uErr) / log(meshRate)
+        q_rate = log(prev_qErr / L2_qErr) / log(meshRate)
         prev_uErr, prev_qErr = L2_uErr, L2_qErr
+        print(f'level: {level}')
         print(f"uh L2-error: {L2_uErr:.3E}, uh conv rate: {u_rate:.2E}")
         print(f"qh L2-error: {L2_qErr:.3E}, qh conv rate: {q_rate:.2E}")
         print('==============================')
